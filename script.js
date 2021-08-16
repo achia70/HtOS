586a8b677864fc46cdeea1df993ef511dbd58cf4
achia70 <65407363+achia70@users.noreply.github.com>
2021-08-16 18:39:42 +0700

    Update and rename script.html to script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$( function() {
+    $( "#menu" ).menu();
+    $( "#clock-dialog" ).dialog();
+    $( "#clock-dialog" ).dialog( "close" );
+    $( "#clock-opener" ).on( "click", function() {
+        $( "#clock-dialog" ).dialog( "open" );
+    });
+    $( "#calculator-dialog" ).dialog();
+    $( "#calculator-dialog" ).dialog( "close" );
+    $( "#calculator-opener" ).on( "click", function() {
+        $( "#calculator-dialog" ).dialog( "open" );
+    });
+} );
