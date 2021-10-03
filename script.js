2cb377cd1729a82ef73f0ea475dbd2013151c868
achia70 <65407363+achia70@users.noreply.github.com>
2021-10-03 17:34:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 $( function() {
     $('#dialog').load('dialog.html');
     $( "#menu" ).menu();
-    $( "#clock-dialog" ).dialog({
-        width: 500
-        height: 500
-    });
+    $( "#clock-dialog" ).dialog();
     $( "#clock-dialog" ).dialog( "close" );
     $( "#clock-opener" ).on( "click", function() {
         $( "#clock-dialog" ).dialog( "open" );
     });
-    $( "#calculator-dialog" ).dialog({
-        width: 500
-        height: 500
-    });
+    $( "#calculator-dialog" ).dialog();
     $( "#calculator-dialog" ).dialog( "close" );
     $( "#calculator-opener" ).on( "click", function() {
         $( "#calculator-dialog" ).dialog( "open" );
